0b9bcbb56e0931ccba2a1577bca7a8e227ee751b
prithish2435 <prithishm25@gmail.com>
2026-08-24 07:23:25 +0530

    Align production build with Node 22

--- src/components/common/PageHero.tsx
+++ src/components/common/PageHero.tsx
@@ -1,3 +1,5 @@
+import type { CSSProperties } from "react";
+
 type PageHeroProps = {
   image: string;
   alt: string;
@@ -21,7 +23,7 @@ export function PageHero({
           {
             "--mobile-position": mobilePosition,
             "--desktop-position": desktopPosition,
-          } as React.CSSProperties
+          } as CSSProperties
         }
       />
     </section>
--- src/components/common/ScrollToTop.tsx
+++ src/components/common/ScrollToTop.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export const ScrollToTop: React.FC = () => {
+export const ScrollToTop = () => {
   const { pathname, hash } = useLocation();
 
   useEffect(() => {
